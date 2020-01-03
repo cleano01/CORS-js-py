991fa7f27937c12582221b65eec686ed7c395909
cleano01 <cleano.ferreira@dce.ufpb.br>
2020-01-03 09:53:52 -0300

    (cors-py) adicionado cors no FastApi

--- CORS-PY/routers/book.py
+++ CORS-PY/routers/book.py
@@ -8,7 +8,7 @@ async def read_root():
     return {r.text}
 
 
-@router.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@router.get("/book_py")
+async def read_item():
+    return {"book": "list py"}
 
